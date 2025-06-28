8031d8c3780935c5d6d8cfbeb15b2a246d279acd
sonhavietnamese <nguyensonha.hanz@gmail.com>
2025-06-28 16:32:28 +0700

    feat: bvh

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,29 +1,60 @@
-import { Canvas } from '@react-three/fiber'
+import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
 import prototype from '@/assets/textures/prototype.png'
 import { useTexture } from '@react-three/drei'
 import * as THREE from 'three'
-import Gor from './components/Gor'
+import Gor from './components/gor'
+import sampleHealthbar from '@/assets/sample-healthbar.png'
+import sampleLeaderboard from '@/assets/sample-leaderboard.png'
+import { useRef } from 'react'
 
 export default function App() {
   return (
     <main className='w-dvw h-dvh'>
+      <Hud />
       <Canvas>
-        <ambientLight intensity={0.5} />
+        <ambientLight intensity={2} />
+
         <directionalLight position={[1, 1, 1]} intensity={1} />
 
         <color attach='background' args={['#000']} />
-        <fog attach='fog' args={['#000', 10, 120]} />
+        {/* <fog attach='fog' args={['#000', 10, 120]} /> */}
 
-        <PerspectiveCamera makeDefault position={[-30, 10, 30]} fov={25} />
+        <Camera />
 
         <Ground />
 
-        <Gor scale={15} />
+        <Gor scale={15} position={[0, -0.5, 0]} />
 
         <OrbitControls />
       </Canvas>
     </main>
+  )
+}
+
+function Camera() {
+  const ref = useRef<THREE.PerspectiveCamera>(null)
+  // const count = useRef(0)
+
+  // useFrame(() => {
+  //   if (ref.current) {
+  //     console.log(ref.current.position)
+  //   }
+  // })
+
+  return <PerspectiveCamera ref={ref} makeDefault position={[-30, 10, 30]} fov={25} />
+}
+
+function Hud() {
+  return (
+    <section className='w-full h-full flex flex-col pointer-events-none items-center justify-center absolute top-0 left-0 z-[1]'>
+      <div className='absolute top-5 left-1/2 -translate-x-1/2'>
+        <img src={sampleHealthbar} draggable={false} className='w-[600px]' />
+      </div>
+      <div className='absolute top-10 right-10'>
+        <img src={sampleLeaderboard} draggable={false} className='w-[400px]' />
+      </div>
+    </section>
   )
 }
 
