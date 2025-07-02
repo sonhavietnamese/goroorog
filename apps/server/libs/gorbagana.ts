0a254510912ff48d5c1831d02d2e961734aeee9a
sonhavietnamese <nguyensonha.hanz@gmail.com>
2025-07-02 13:20:50 +0700

    feat: ASLDK)(*#$%^&*)

--- apps/server/libs/gorbagana.ts
+++
@@ -1,4 +0,0 @@
-import { Connection } from '@solana/web3.js'
-
-export const GORBAGANA_RPC_URL = 'https://rpc.gorbagana.wtf'
-export const GORBAGANA_CONNECTION = new Connection(GORBAGANA_RPC_URL, { commitment: 'finalized' })
